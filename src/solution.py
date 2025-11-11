f4f2d291c0534e980b5ca4c1528c4e1090b263e8
odsbaron <871031393@qq.com>
2025-11-11 23:09:38 +0800

    Accelerate rolling_regbeta operator 2

--- src/solution.py
+++ src/solution.py
@@ -1,25 +1,18 @@
 import polars as pl
 import numpy as np
-
+pl.Config.set_streaming_chunk_size(50_000_000)
 class ops:
     @staticmethod
     def rolling_regbeta(col_x_or_expr, col_y_or_expr, window: int) -> pl.Expr:
-        if isinstance(col_x_or_expr, str):
-            expr_x = pl.col(col_x_or_expr)
-        else:
-            expr_x = col_x_or_expr
+        expr_x = col_x_or_expr if isinstance(col_x_or_expr, pl.Expr) else pl.col(col_x_or_expr)
+        expr_y = col_y_or_expr if isinstance(col_y_or_expr, pl.Expr) else pl.col(col_y_or_expr)
 
-        if isinstance(col_y_or_expr, str):
-            expr_y = pl.col(col_y_or_expr)
-        else:
-            expr_y = col_y_or_expr
-    
-  
+
         cov_xy = pl.rolling_cov(expr_x, expr_y, window_size=window, ddof=1, min_samples=2) 
         var_x = expr_x.rolling_var(window_size=window, ddof=1, min_samples=2)
         
         # 参数微调
-        return pl.when(var_x < 1.02*1e-6).then(0.0).otherwise(cov_xy / var_x).alias("rolling_regbeta")
+        return pl.when(var_x < 1*1e-6).then(0.0).otherwise(cov_xy / var_x).alias("rolling_regbeta")
 
 
 def ops_rolling_regbeta(input_path: str, window: int = 20) -> np.ndarray:
@@ -27,9 +20,9 @@ def ops_rolling_regbeta(input_path: str, window: int = 20) -> np.ndarray:
         pl.scan_parquet(input_path)
         .with_columns([
             pl.col("Close").cast(pl.Float64),
-            pl.col("Low").cast(pl.Float64),
+            pl.col("Low").cast(pl.Float64),  
             pl.col("symbol").cast(pl.Categorical)  # <-- 添加在这里
-        ])
+      ])
         .select(
             ops.rolling_regbeta("Low", "Close", window).over("symbol")
         )
